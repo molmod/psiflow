4e33670982ca21e87d1ee9f47889297ccd522288
svandenhaute <svdenhau@gmail.com>
2023-03-03 18:56:42 +0100

    allow heterogeneous dataset to be evaluated with single calculator

--- psiflow/models/base.py
+++ psiflow/models/base.py
@@ -42,12 +42,10 @@ def evaluate_dataset(
         torch.set_default_dtype(torch.float32)
     dataset = read_dataset(slice(None), inputs=[inputs[0]])
     if len(dataset) > 0:
-        atoms = dataset[0].copy()
-        atoms.calc = load_calculator(inputs[1].filepath, device, dtype)
-        for _atoms in dataset:
-            _atoms.calc = None
-            atoms.set_positions(_atoms.get_positions())
-            atoms.set_cell(_atoms.get_cell())
+        calculator = load_calculator(inputs[1].filepath, device, dtype)
+        for atoms in dataset:
+            calculator.reset()
+            atoms.calc = calculator
             energy = atoms.get_potential_energy()
             forces = atoms.get_forces()
             try: # some models do not have stress support
@@ -55,9 +53,10 @@ def evaluate_dataset(
             except Exception as e:
                 print(e)
                 stress = np.zeros((3, 3))
-            _atoms.info['energy'] = energy
-            _atoms.info['stress'] = stress
-            _atoms.arrays['forces'] = forces
+            atoms.info['energy'] = energy
+            atoms.info['stress'] = stress
+            atoms.arrays['forces'] = forces
+            atoms.calc = None
         save_dataset(dataset, outputs=[outputs[0]])
 
 
