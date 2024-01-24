073e6954cf9b1f3b80c46f730cabc176e80da93e
svdenhau <svdenhau@gmail.com>
2024-01-24 13:22:57 +0100

    fix failing test

--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -42,8 +42,8 @@ restraint: RESTRAINT ARG=CV1 AT=150 KAPPA=1
         reference,
         walkers,
         identifier,
-        error_thresholds_for_reset=(2000, 1e9),
-        error_thresholds_for_discard=(2000, 1e9),
+        error_thresholds_for_reset=(1e9, 1e9),
+        error_thresholds_for_discard=(1e9, 1e9),
         metrics=metrics,
     )
     assert data.length().result() == 6  # one state should have failed
