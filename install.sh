fa312be961a77a674fd7c950e5b844e75cda3231
svdenhau <svdenhau@gmail.com>
2024-05-02 10:37:21 +0200

    install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+export MAMBA_ROOT_PREFIX=$(pwd)  # optional, defaults to ~/micromamba
+
+eval "$(./bin/micromamba shell hook -s posix)"
+micromamba activate
+micromamba create -n _psiflow_env -y python=3.10 pip ndcctools -c conda-forge
+micromamba activate _psiflow_env
+pip install git+https://github.com/molmod/psiflow.git@ipi
+
+# create activate.sh
+echo "cd $(pwd)" >> activate.sh
+echo "export MAMBA_ROOT_PREFIX=$(pwd)" >> activate.sh
+echo 'eval "$(./bin/micromamba shell hook -s posix)"' >> activate.sh
+echo "micromamba activate _psiflow_env" >> activate.sh
