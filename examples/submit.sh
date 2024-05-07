a9d3c3d93f02e858f9facfd587e75dea90627513
svdenhau <svdenhau@gmail.com>
2024-05-07 10:25:17 +0200

    new PIMD example

---
+++ examples/submit.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# List of filenames
+files=(
+	"formic_acid_transition.py"
+	"h2_static_dynamic.py"
+	"iron_bulk_modulus.py"
+	"water_cp2k_noise.py"
+	"iron_thermodynamic_integration.py"
+    "water_path_integral_md.py"
+)
+
+run_dir=$(pwd)/run_examples
+mkdir $run_dir && cp lumi.yaml $run_dir && cd $run_dir
+
+# Loop over each filename
+for filename in "${files[@]}"
+do
+    name="${filename%.*}"
+    mkdir $name
+    cp lumi.yaml $name
+
+    cat > $name/job.sh <<EOF
+#!/bin/bash
+#
+#SBATCH -p small
+#SBATCH --account=project_465000847
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --job-name=$name
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH -e error.txt
+#SBATCH -o output.txt
+
+
+curl -LJO https://github.com/molmod/psiflow/archive/ipi.zip
+unzip -j psiflow-ipi.zip "psiflow-ipi/examples/data/*" -d data
+
+curl -O https://raw.githubusercontent.com/molmod/psiflow/ipi/examples/$filename
+python $filename lumi.yaml
+EOF
+
+    cd $name
+    sbatch job.sh
+    cd $run_dir
+
+done
---
+++ examples/water_path_integral_md.py
@@ -0,0 +1,50 @@
+import numpy as np
+from ase.neighborlist import primitive_neighbor_list
+
+import psiflow
+from psiflow.data import Dataset
+from psiflow.hamiltonians import get_mace_mp0
+from psiflow.sampling import Walker, sample
+
+
+def get_OH_distances(geometries):
+    distances = []
+    for geometry in geometries:
+        d = primitive_neighbor_list(
+            "d",
+            3 * [True],
+            geometry.cell,
+            geometry.per_atom.positions,
+            cutoff={("O", "H"): 1.5},
+            numbers=geometry.per_atom.numbers,
+        )
+        distances.append(d)
+    min_n_distances = min([len(d) for d in distances])
+    return np.array([d[:min_n_distances] for d in distances]).flatten()
+
+
+def main():
+    geometry = Dataset.load("data/h2o_64.xyz")[0]
+    mace = get_mace_mp0()
+
+    trajectories = []
+    for i in range(6):
+        walker = Walker(
+            geometry,
+            mace,
+            nbeads=2**i,
+            temperature=300,
+        )
+        outputs = sample([walker], steps=5000, step=20)
+        trajectories.append(outputs[0].trajectory)
+
+    for i, trajectory in enumerate(trajectories):
+        distances = get_OH_distances(trajectory.geometries().result())
+        nbeads = 2**i
+        std = np.std(distances)
+        print("nbeads = {} --> std(O-H) = {} A".format(nbeads, std))
+
+
+if __name__ == "__main__":
+    with psiflow.load():
+        main()
