d3b4543f07acb15b07674ae63f7c8af87ef5b510
Sander Vandenhaute <svdenhau@gmail.com>
2024-06-01 12:56:31 -0400

    update install and build_containers with ipi to main merge

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
-export MAMBA_ROOT_PREFIX=$(pwd)  # optional, defaults to ~/micromamba
+export MAMBA_ROOT_PREFIX=$(pwd) # optional, defaults to ~/micromamba
 
 eval "$(./bin/micromamba shell hook -s posix)"
 micromamba activate
-micromamba create -n _psiflow_env -y python=3.10 pip ndcctools -c conda-forge
+micromamba create -n _psiflow_env -y python=3.10 pip ndcctools=7.11.0 -c conda-forge
 micromamba activate _psiflow_env
-pip install git+https://github.com/molmod/psiflow.git@ipi
+pip install git+https://github.com/molmod/psiflow
 
 # create activate.sh
-echo 'ORIGDIR=$PWD' >> activate.sh  # prevent variable substitution
-echo "cd $(pwd)" >> activate.sh
-echo "export MAMBA_ROOT_PREFIX=$(pwd)" >> activate.sh
-echo 'eval "$(./bin/micromamba shell hook -s posix)"' >> activate.sh
-echo "micromamba activate _psiflow_env" >> activate.sh
-echo 'cd $ORIGDIR' >> activate.sh   # prevent variable substitution
+echo 'ORIGDIR=$PWD' >>activate.sh # prevent variable substitution
+echo "cd $(pwd)" >>activate.sh
+echo "export MAMBA_ROOT_PREFIX=$(pwd)" >>activate.sh
+echo 'eval "$(./bin/micromamba shell hook -s posix)"' >>activate.sh
+echo "micromamba activate _psiflow_env" >>activate.sh
+echo 'cd $ORIGDIR' >>activate.sh # prevent variable substitution
