356b10c1556a0dc485220ae6c5607da788daf732
pdobbelaere <pidobbel.Dobbelaere@UGent.be>
2025-08-05 19:21:33 +0200

    Update __init__.py

--- psiflow/reference/__init__.py
+++ psiflow/reference/__init__.py
@@ -2,5 +2,5 @@
 from .cp2k_ import CP2K  # noqa: F401
 from .dftd3_ import D3  # noqa: F401
 from .gpaw_ import GPAW  # noqa: F401
-from .orca_ import ORCA  # noqa: F401
-from .reference import Reference, evaluate  # noqa: F401
+from .orca_ import ORCA, create_input_template  # noqa: F401
+from .reference import Reference  # noqa: F401
