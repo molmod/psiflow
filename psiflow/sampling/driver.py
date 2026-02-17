aa385ddabdeb8d6479b017f85d3efc23cdd22707
pdobbelaere <pidobbel.Dobbelaere@UGent.be>
2026-02-17 21:50:11 +0100

    use built-in custom driver support

--- psiflow/sampling/driver.py
+++ psiflow/sampling/driver.py
@@ -1,6 +1,5 @@
 """
 Updated version of the Psiflow driver included in i-Pi
-TODO: once this is stable, PR into i-Pi again
 """
 
 import numpy as np
@@ -9,11 +8,20 @@ from ipi.pes.dummy import Dummy_driver
 from ipi.utils.units import unit_to_internal, unit_to_user
 from ipi.utils.messages import warning
 
-read = None
-Geometry = None
-function_from_json = None
-initialise_driver = None
-check_output = None
+try:
+    from ase.io import read
+    from psiflow.geometry import Geometry
+    from psiflow.functions import function_from_json, Function
+    from psiflow.sampling.utils import initialise_driver, check_output
+except ImportError as e:
+    message = (
+        "Could not find the Psiflow driver dependencies, "
+        "make sure they are installed with "
+        "`pip install git+https://github.com/molmod/psiflow.git`."
+    )
+    warning(f"{message}: {e}")
+    raise ImportError(message) from e
+
 
 __DRIVER_NAME__ = "psiflow"
 __DRIVER_CLASS__ = "Psiflow_driver"
@@ -33,47 +41,29 @@ class Psiflow_driver(Dummy_driver):
         :param hamiltonian: str, filename of a JSON file defining the Psiflow function
     """
 
-    def __init__(self, template, hamiltonian, *args, **kwargs):
-        global read, Geometry, function_from_json, initialise_driver, check_output
-        if (
-            read is None
-            or Geometry is None
-            or function_from_json is None
-            or initialise_driver is None
-            or check_output is None
-        ):
-            try:
-                from ase.io import read
-                from psiflow.geometry import Geometry
-                from psiflow.functions import function_from_json
-                from psiflow.sampling.utils import initialise_driver, check_output
-            except ImportError as e:
-                message = (
-                    "Could not find the Psiflow driver dependencies, "
-                    "make sure they are installed with "
-                    "`pip install git+https://github.com/molmod/psiflow.git`."
-                )
-                warning(f"{message}: {e}")
-                raise ImportError(message) from e
+    template: str
+    hamiltonian: str
+    geometry: Geometry
+    function: Function
 
+    def __init__(self, template, hamiltonian, *args, **kwargs):
         self.template = template
         self.hamiltonian = hamiltonian
-        self.template_geometry = self.function = None
         super().__init__(*args, **kwargs)
 
     def check_parameters(self):
-        self.template_geometry = Geometry.from_atoms(read(self.template))
+        self.geometry = Geometry.from_atoms(read(self.template))
         self.function = function_from_json(self.hamiltonian, **self.kwargs)
         initialise_driver(self)
 
     def compute_structure(self, cell, pos):
         pos = unit_to_user("length", "angstrom", pos)
         cell = unit_to_user("length", "angstrom", cell.T)
-        self.template_geometry.per_atom.positions[:] = pos
-        if self.template_geometry.periodic:
-            self.template_geometry.cell[:] = cell
+        self.geometry.per_atom.positions[:] = pos
+        if self.geometry.periodic:
+            self.geometry.cell[:] = cell
 
-        outputs = self.function(self.template_geometry)
+        outputs = self.function(self.geometry)
         check_output(self, outputs)
 
         # converts to internal quantities
@@ -84,7 +74,7 @@ class Psiflow_driver(Dummy_driver):
             unit_to_internal("energy", "electronvolt", energy), np.float64
         )
         force_ipi = np.asarray(unit_to_internal("force", "ev/ang", forces), np.float64)
-        vir_calc = -stress * self.template_geometry.volume
+        vir_calc = -stress * self.geometry.volume
         vir_ipi = np.array(
             unit_to_internal("energy", "electronvolt", vir_calc.T), dtype=np.float64
         )
--- psiflow/sampling/utils.py
+++ psiflow/sampling/utils.py
@@ -1,17 +1,12 @@
+import os
+import time
 from typing import Any, Optional
 
 import numpy as np
-import typeguard
 from ase.data import chemical_symbols
-
-from psiflow.functions import Function
 
 # do not use psiflow apps; parsl config is not loaded in this process!
 from psiflow.geometry import Geometry
-
-# only import stuff which does not issue useless warnings; otherwise
-# python -c 'import .client; print(client.__file__)' is going to be polluted
-# with those import-related warnings
 
 
 class ForceMagnitudeException(Exception):
@@ -26,7 +21,11 @@ def timeout_handler(signum, frame):
     raise TimeoutException
 
 
-@typeguard.typechecked
+def create_xml_list(items: list[str]) -> str:
+    """Pure helper"""
+    return " [ {} ] ".format(", ".join(items))
+
+
 def check_forces(
     forces: np.ndarray,
     geometry: Any,
@@ -36,79 +35,41 @@ def check_forces(
         geometry = Geometry.from_atoms(geometry)
 
     exceeded = np.linalg.norm(forces, axis=1) > max_force
-    if np.sum(exceeded):
-        indices = np.arange(len(geometry))[exceeded]
-        numbers = geometry.per_atom.numbers[exceeded]
-        symbols = [chemical_symbols[n] for n in numbers]
-        raise ForceMagnitudeException(
-            "\nforce exceeded {} eV/A for atoms {}"
-            " with chemical elements {}\n".format(
-                max_force,
-                indices,
-                symbols,
-            )
+    if not np.sum(exceeded):
+        return
+    indices = np.arange(len(geometry))[exceeded]
+    numbers = geometry.per_atom.numbers[exceeded]
+    symbols = [chemical_symbols[n] for n in numbers]
+    raise ForceMagnitudeException(
+        "\nforce exceeded {} eV/A for atoms {}"
+        " with chemical elements {}\n".format(
+            max_force,
+            indices,
+            symbols,
         )
-    else:
-        pass
+    )
 
 
-class FunctionDriver:
-
-    def __init__(
-        self,
-        template: Geometry,
-        function: Function,
-        max_force: Optional[float],
-        verbose: bool = True,  # used by i-PI internally?
-        error_msg="",
-    ):
-        self.verbose = verbose
-        self.template = template
-        self.function = function
-        self.max_force = max_force
-
-    def check_arguments(self):
-        pass
-
-    def __call__(self, cell, pos):
-        from ipi.utils.units import unit_to_internal, unit_to_user
-
-        pos = unit_to_user("length", "angstrom", pos)
-        cell = unit_to_user("length", "angstrom", cell.T)
-
-        self.template.per_atom.positions[:] = pos
-        if self.template.periodic:
-            self.template.cell[:] = cell
-
-        outputs = self.function(self.template)
-        energy = outputs["energy"]
-        forces = outputs["forces"]
-        stress = outputs["stress"]
-
-        # check for max_force
-        if self.max_force is not None:
-            check_forces(forces, self.template, self.max_force)
-
-        # converts to internal quantities
-        pot_ipi = np.asarray(
-            unit_to_internal("energy", "electronvolt", energy), np.float64
-        )
-        force_ipi = np.asarray(unit_to_internal("force", "ev/ang", forces), np.float64)
-        vir_calc = -stress * self.template.volume
-        vir_ipi = np.array(
-            unit_to_internal("energy", "electronvolt", vir_calc.T), dtype=np.float64
-        )
-        extras = ""
-
-        return pot_ipi, force_ipi, vir_ipi, extras
+def initialise_driver(driver: 'Psiflow_driver') -> None:
+    """"""
+    function = driver.function
+    name = function.__class__.__name__
+    affinity = os.sched_getaffinity(os.getpid())
+    t0 = time.time()
+    for _ in range(10):
+        function(driver.geometry)  # torch warm-up before simulation
+    t1 = time.time()
+    msg = [
+        "- Psiflow -",
+        f"Initialising driver for {name} with options {driver.kwargs}",
+        f"CPU affinity [PID {os.getpid()}]: {affinity}",
+        f"Time for 10 evaluations: {t1 - t0:.3f}",
+        "- - - - - -",
+    ]
+    print("\n".join(msg))
 
 
-def initialise_driver(driver) -> None:
-    """This function does not do anything yet. Stay tuned."""
-    pass
-
-
-def check_output(data: dict) -> None:
-    """This function does not do anything yet. Stay tuned."""
-    pass
-
+def check_output(driver: 'Psiflow_driver', data: dict) -> None:
+    """"""
+    if max_force := driver.kwargs.get("max_force"):
+        check_forces(data["forces"], driver.geometry, max_force)
